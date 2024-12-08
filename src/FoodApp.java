630d181e70a63d5c082c69e13b3dac278e51166f
AndRoo1 <148553704+AndRoo1@users.noreply.github.com>
2024-12-08 22:49:43 +0000

    Add files via upload

---
+++ src/FoodApp.java
@@ -0,0 +1,11 @@
+/*
+ *FoodApp.java
+ *@author Andrew Cleary
+ *27/11/24
+ */
+public class FoodApp {
+    public static void main(String[] args) {
+        FoodHomePage MyFoodHomePage = new FoodHomePage();
+        //Scaling Scaling = new Scaling();
+    }
+}
